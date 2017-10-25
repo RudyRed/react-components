6df3d9065ad01a9608572a2007074582470a6382
RudyRed <willgross94@gmail.com>
2017-10-25 11:50:56 -0700

    props

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 
-var Kale = () => (
-  <li>Kale</li>
-)
-
-var Broccoli = () => (
-  <li>Broccoli</li>
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.item[0]}</li>
+    <li>{props.item[1]}</li>
+  </ul>
 )
 
 var GroceryList = () => (
-  <ul>
-    <Kale />
-    <Broccoli />
-  </ul>
+  <div>
+    <h2>GroceryList</h2>
+    <GroceryListItem item={['Kale', 'Jam']}/>
+  </div>
 )
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<GroceryList />, document.getElementById("GroceryList"));
+
+
+
+
+//
+// // Here we create a `TodoList` component
+// var TodoList = (props) => (
+//   <ul>
+//     <li>{props.todos[0]}</li>
+//     <li>{props.todos[1]}</li>
+//     <li>{props.todos[2]}</li>
+//   </ul>
+// );
+//
+// var App = () => (
+//   <div>
+//     <h2>My Todo List</h2>
+//     <TodoList todos={['Learn React', 'Crush Recast.ly', 'Maybe sleep']}/>
+//   </div>
+// );
+// ReactDOM.render(<App />, document.getElementById('app'))
