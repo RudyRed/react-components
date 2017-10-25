fa78580038e3cb5ea2b43a8b7f260d8fa10a38ab
RudyRed <willgross94@gmail.com>
2017-10-25 11:18:21 -0700

    Grocery List

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+
+var Kale = () => (
+  <li>Kale</li>
+)
+
+var Broccoli = () => (
+  <li>Broccoli</li>
+)
+
+var GroceryList = () => (
+  <ul>
+    <Kale />
+    <Broccoli />
+  </ul>
+)
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
